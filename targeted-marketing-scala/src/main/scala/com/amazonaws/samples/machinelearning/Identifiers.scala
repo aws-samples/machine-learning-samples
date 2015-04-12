3c432b43ffc7da2aad16c2a1cd5aa8b4da581910
Filippo Vitale <fvitale@gmail.com>
2015-04-12 17:07:08 +1000

    housekeeping

--- targeted-marketing-scala/src/main/scala/com/amazonaws/samples/machinelearning/Identifiers.scala
+++ targeted-marketing-scala/src/main/scala/com/amazonaws/samples/machinelearning/Identifiers.scala
@@ -12,7 +12,7 @@ object Identifiers {
   private def base62RandomChar(): Char = BASE62_CHARS(Random.nextInt(BASE62_CHARS.length))
 
   private def generateEntityId(prefix: String): String = {
-    val rand = List.fill(11)(base62RandomChar()).map(_.toString).mkString
+    val rand = List.fill(11)(base62RandomChar()).mkString
     s"$prefix-$rand"
   }
 
